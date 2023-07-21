fe7e806cbb5bde05ae6f3c34b8cb8b5a21f5e9b1
Anandh Kumar <accetanandhmca@gmail.com>
2023-07-21 11:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,112 @@
+//Design Part
+var containerDiv = createDivWithId('container');
+var headingDiv = document.createElement('div');
 
+headingDiv.className= 'heading';
+document.body.append(headingDiv);
+
+var titleHeading = document.createElement('h1');
+titleHeading.id = 'title';
+titleHeading.innerText = 'Calculator';
+
+var descriptionPara = document.createElement('p');
+descriptionPara.id = 'description';
+descriptionPara.innerText = 'The Calculator using Html, CSS, and JavaScript that perform functions of Addition, Multiplication, Subtraction, Division and Modulus operations using DOM element with responsive';
+
+var calculatorDiv = createDivWithId('calculator');
+
+var resultWindowDiv = createDivWithId('result-window');
+
+var historyDiv = createDivWithId('history');
+
+var historyPara = document.createElement('p');
+historyPara.id = 'history-value';
+
+var outputDiv = document.createElement('div');
+outputDiv.className = 'output';
+outputDiv.id = 'output';
+document.body.append(outputDiv);
+
+var resultInput = document.createElement('input');
+resultInput.setAttribute("id","result")
+document.body.append(resultInput);
+
+var KeyboardDiv = createDivWithId('keyboard');
+
+var clearButton = createButtonWithClassAndId('operator','clear','C');
+var backSpaceButton = createButtonWithClassAndId('operator','backspace','⌫');
+var modulusButton = createButtonWithClassAndId('operator','modulus','%');
+var divideButton = createButtonWithClassAndId('operator','divide','&#247;');
+var numberSevenButton = createButtonWithClassAndId('number','7','7');
+var numberEightButton = createButtonWithClassAndId('number','8','8');
+var numberNineButton = createButtonWithClassAndId('number','9','9');
+var multiplyButton = createButtonWithClassAndId('operator','multiple','&times;');
+var numberFourButton = createButtonWithClassAndId('number','4','4');
+var numberFiveButton = createButtonWithClassAndId('number','5','5');
+var numberSixButton = createButtonWithClassAndId('number','6','6');
+var subtractButton = createButtonWithClassAndId('operator','subtract','-');
+var numberOneButton = createButtonWithClassAndId('number','1','1');
+var numberTwoButton = createButtonWithClassAndId('number','2','2');
+var numberThreeButton = createButtonWithClassAndId('number','3','3');
+var addButton = createButtonWithClassAndId('operator','add','+');
+var numbeZeroButton =  createButtonWithClassAndId('number','0','0');
+var numbedoubleZeroButton =  createButtonWithClassAndId('number','00','00');
+var dotButton = createButtonWithClassAndId('dot','dot','.');
+var equalButton = createButtonWithClassAndId('operator','equal','=');
+
+headingDiv.appendChild(titleHeading);
+headingDiv.appendChild(descriptionPara);
+
+calculatorDiv.appendChild(resultWindowDiv);
+calculatorDiv.appendChild(KeyboardDiv);
+
+resultWindowDiv.appendChild(historyDiv);
+historyDiv.appendChild(historyPara);
+
+resultWindowDiv.appendChild(outputDiv);
+outputDiv.appendChild(resultInput);
+
+KeyboardDiv.appendChild(clearButton);
+KeyboardDiv.appendChild(backSpaceButton);
+KeyboardDiv.appendChild(modulusButton);
+KeyboardDiv.appendChild(divideButton);
+KeyboardDiv.appendChild(numberSevenButton);
+KeyboardDiv.appendChild(numberEightButton);
+KeyboardDiv.appendChild(numberNineButton);
+KeyboardDiv.appendChild(multiplyButton);
+KeyboardDiv.appendChild(numberFourButton);
+KeyboardDiv.appendChild(numberFiveButton);
+KeyboardDiv.appendChild(numberSixButton);
+KeyboardDiv.appendChild(subtractButton);
+KeyboardDiv.appendChild(numberOneButton);
+KeyboardDiv.appendChild(numberTwoButton);
+KeyboardDiv.appendChild(numberThreeButton);
+KeyboardDiv.appendChild(addButton);
+KeyboardDiv.appendChild(numbeZeroButton);
+KeyboardDiv.appendChild(numbedoubleZeroButton);
+KeyboardDiv.appendChild(dotButton);
+KeyboardDiv.appendChild(equalButton);
+
+containerDiv.appendChild(headingDiv);
+containerDiv.appendChild(calculatorDiv);
+
+function createDivWithId(IdValue) {
+    var divElement = document.createElement('div');
+    divElement.id=(IdValue);
+    document.body.append(divElement);
+    return divElement;
+}
+
+function createButtonWithClassAndId(className,IdValue,content){
+    var buttonElement = document.createElement('button');
+    buttonElement.className = (className);
+    buttonElement.id = (IdValue);
+    buttonElement.innerHTML= content;
+    document.body.append(buttonElement);
+    return buttonElement;
+}
+
+//Functional Part
 var number = document.getElementsByClassName("number");
 for(var i =0;i<number.length;i++){
 	number[i].addEventListener('click',function(){
@@ -150,4 +258,3 @@ function getFormattedNumber(num){
 function reverseNumberFormat(num){
 	return Number(num.replace(/,/g,''));
 }
-
